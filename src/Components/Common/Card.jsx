8346b47f6c3bfa0a9389cd3f5bb960394443b2e4
Karan <karanmohan44@gmail.com>
2024-09-17 18:21:29 +0530

    bug fixes

--- src/Components/Common/Card.jsx
+++ src/Components/Common/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({item}) => {
     <div className='w-60 h-56 bg-white rounded-md overflow-hidden '>
          <img className='w-full object-cover h-32' src={item.backdrop_path ? `https://image.tmdb.org/t/p/original${item.backdrop_path}`: image }/>
          <p className='capitalize font-semibold text-lg text-start px-2'>{item.title || item.original_title || item.name}</p>
-         <p className='text-center text-xs'>{ item.overview && item.overview.slice(0,70)} <Link className="text-blue-500">...more</Link></p>
+         <p className='text-center text-xs'>{ item.overview ? item.overview.slice(0,70) : "not available"} <Link className="text-blue-500">...more</Link></p>
     </div>
     </>
   )
